a249af8d1c1af2a4ccf9fde3fcf029b86596ef94
Olha Klochak <olga.klochak@gmail.com>
2023-06-05 18:28:10 +0300

    commit form

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,17 +1,17 @@
+import Util.Util;
 import config.BaseClass1;
-import config.ScrollToelement;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.*;
 
-import javax.rmi.CORBA.Util;
 import java.util.List;
 
 public class FirstTest extends BaseClass1 {
     By course = new By.ByXPath("//button[@data-dropdown-trigger='coursesMenu']");
     By search = new By.ByXPath("//button[@data-category='programming']");
-    By list = new By.ByXPath("//button[@class='button -xl -light-blue coaches_action']");
+    By list = new By.ByXPath("//button[@id='coachesShowAllButton']");
     By coachList = new By.ByXPath("//p[@class='coach-card_name']");
+    By Fomenko = new By.ByXPath("//p[text()='Андрій Фоменко']");
 
 
     @BeforeClass
@@ -20,23 +20,23 @@ public class FirstTest extends BaseClass1 {
     }
 
     @Test
-    public void test() throws InterruptedException {
+    public void test1() throws InterruptedException {
         driver.findElement(course).click();
         driver.findElement(search).click();
         driver.get("https://ithillel.ua/courses/front-end-basic");
-        ScrollToelement(driver, By.xpath("//ul[@class='coach-list coaches_list']"));
-        // driver.findElement(list).click();
+        Util.ScrollToelement(driver,By.xpath("//button[@id='coachesShowAllButton']"));
+        driver.findElement(By.xpath("//button[@id='coachesShowAllButton']")).click();
+
         List<WebElement> elements = driver.findElements(By.xpath("//p[@class='coach-card_name']"));
-        System.out.println(elements);
-        System.out.println(driver.findElement(coachList).getText());
+        for (WebElement element : elements) {
+            System.out.println(element.getText());
+
+            Thread.sleep(5000);
+        }
 
 
-        Thread.sleep(5000);
     }
-
-
-    }
-
+}
   //  @Test
    // public void test3() {
     //    driver.get("https://ithillel.ua/courses/front-end-basic");
